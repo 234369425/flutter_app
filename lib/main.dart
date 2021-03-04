ce90fdd06591cedb286fa4da191b5f25dce6260f
wangdi <wangdi10@xiaomi.com>
2021-03-04 19:40:37 +0800

    增加RTM sdk

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,24 @@ import 'package:flutter_app/provider/login_model.dart';
 import 'package:flutter_app/utils/route.dart';
 import 'package:provider/provider.dart';
 
+import 'agora/agora_rtm_client.dart';
+
 void main() {
   runApp(ProviderConfig.getInstance().getLoginPage(Application()));
 }
 
 class Application extends StatelessWidget {
+  var _client;
+
+  Application() {
+    init();
+  }
+
+  init() async {
+    _client =
+        await AgoraRtmClient.createInstance('9c2be3809d414367a9eac783c3621d72');
+  }
+
   @override
   Widget build(BuildContext context) {
     final model = Provider.of<LoginModel>(context)..setContext(context);
